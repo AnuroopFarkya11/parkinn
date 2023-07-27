b275e313751cc5cca89367a8f4be68634a6a943b
anuroop <anuroopf02@gmail.com>
2023-07-27 10:05:09 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/parkinn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parkinn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const ParkInn());
+}
+
+
+class ParkInn extends StatelessWidget {
+  const ParkInn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ParkInn",
+      theme: ThemeData(primarySwatch: Colors.green),
+
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
