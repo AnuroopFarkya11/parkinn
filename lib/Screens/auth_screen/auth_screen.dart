b800ecd8d2d57048bdecf34aea28cb3b88ce3754
anuroop <anuroopf02@gmail.com>
2023-07-28 10:40:10 +0530

    initial commit

--- lib/Screens/auth_screen/auth_screen.dart
+++ lib/Screens/auth_screen/auth_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:parkinn/Utils/brand_color.dart';
+import 'package:parkinn/Utils/sizes.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -12,7 +13,50 @@ class AuthScreen extends StatelessWidget {
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
+            SliverAppBar(
 
+              expandedHeight: CustomSizes.height*0.17,
+              // backgroundColor: Colors.black54,
+              flexibleSpace: LayoutBuilder(
+                builder: (context, constraint) {
+                  return Container(
+                    // color: Colors.blue,
+                    padding: const EdgeInsets.all(12),
+                    height: constraint.maxHeight,
+                    width: constraint.maxWidth,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "ParkInn",
+                          style: TextStyle(
+                              color: BrandColors.primaryColor,
+                              fontSize: constraint.maxHeight * 0.2,
+                              fontWeight: FontWeight.w900),
+                        ),
+                        Row(
+                          children: [
+                            Text("Simple ",style: TextStyle(color: BrandColors.subTitleColor,fontSize: constraint.maxWidth*0.11),),
+                            Text("parking",style: TextStyle(color: BrandColors.subTitleColor,fontSize: constraint.maxWidth*0.11,fontWeight: FontWeight.w900),)
+
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Container(
+
+              ),
+            )
 
 
           ],
