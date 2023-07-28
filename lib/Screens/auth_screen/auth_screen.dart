318e732df0a2e120b6c59acff28a4170070d54ce
daksh <dakshpathak0705@gmail.com>
2023-07-29 01:39:02 +0530

    initial commit

--- lib/Screens/auth_screen/auth_screen.dart
+++ lib/Screens/auth_screen/auth_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:parkinn/Utils/brand_color.dart';
+import 'package:parkinn/Utils/sizes.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -12,9 +13,22 @@ class AuthScreen extends StatelessWidget {
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
-
-
-
+            SliverAppBar(
+              backgroundColor: Colors.black,
+              title: Text(
+                "ParkInn",
+                style: TextStyle(fontSize: 35, color: BrandColors.primaryColor),
+              ),
+              titleSpacing: 40,
+               expandedHeight: CustomSizes.height * 0.19,
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(CustomSizes.height * 0.18),
+                child: Container(
+                  height: CustomSizes.height / 6,
+                  color: Colors.orange,
+                ),
+              ),
+            )
           ],
         ),
       ),
