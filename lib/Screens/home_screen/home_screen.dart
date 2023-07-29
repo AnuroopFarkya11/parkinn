d2deef7879db3ffb3d3ea61354e2dc6dbc02e6aa
anuroop <anuroopf02@gmail.com>
2023-07-29 11:30:13 +0530

    initial commit

---
+++ lib/Screens/home_screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:parkinn/Screens/home_screen/home_controller.dart';
+
+class HomeScreen extends GetView<HomeController>{
+
+  /*
+  * todo : daksh
+  *
+  *
+  *
+  * */
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+
+        slivers: [
+
+
+
+        ],
+
+      ),
+    );
+  }
+
+}
